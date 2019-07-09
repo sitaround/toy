8673c2692b2df05a17dcd7a899337e9425b81d1b
sitaround <sitaround91@gmail.com>
2019-07-09 14:08:55 +0900

    firebase & vuetify use

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import './plugins/firebase'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
 
 new Vue({
   router,
