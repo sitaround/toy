1d6b6e04df7fd39c0518375b5bc54f728a6af465
sitaround <sitaround91@gmail.com>
2019-07-19 17:43:49 +0900

    docs : 사용하지 않는 함수 제거

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,4 +1,3 @@
-import Vue from 'vue'
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
@@ -9,25 +8,12 @@ const FIRESTORE = firebase.firestore()
 const PORTFOLIOS = 'portfolios'
 
 export default {
-  postPortfolio (data) {
+  postPortfolio (title, body) {
     return FIRESTORE.collection(PORTFOLIOS).add({
-      data,
+      title: title,
+      body: body,
       created_at: firebase.firestore.FieldValue.serverTimestamp()
     }).then(response => alert('저장 완료.'))
-      .catch(error => console.log(error))
-  },
-  getPortfolio () {
-    const postsCollection = FIRESTORE.collection(PORTFOLIOS)
-    return postsCollection
-      .orderBy('created_at', 'desc')
-      .get()
-      .then((docSnapshots) => {
-        return docSnapshots.docs.map((doc) => {
-          let data = doc.data()
-          data.created_at = new Date(data.created_at.toDate())
-          return data
-        })
-      })
       .catch(error => console.log(error))
   }
 }
