832537861961de1dbd6c3817c288c914efee5ab2
sitaround <sitaround91@gmail.com>
2019-07-19 17:42:01 +0900

    feat : 날씨 widget component 추가 darksky api를 이용한 weather widget 추가, default 위치는 서울, 접속 위치에 따라 달라짐

---
+++ src/components/Weather.vue
@@ -0,0 +1,26 @@
+<template>
+  <weatherWidget
+    api-key="8183ddac5cfba86ddc36a59aed5b2d87"
+    :latitude="latitude"
+    :longitude="longitude"
+    language="en"
+    units="ca"
+    hide-header>
+  </weatherWidget>
+</template>
+
+<script>
+import VueWeatherWidget from 'vue-weather-widget'
+import 'vue-weather-widget/dist/css/vue-weather-widget.css'
+export default {
+  name: 'Weather',
+  props: {
+    latitude: String,
+    longitude: String
+  },
+  components: {
+    'weatherWidget': VueWeatherWidget
+  }
+}
+
+</script>
