22320b4ddccc84fd019a11d3eb9912672ecf697c
sitaround <sitaround91@gmail.com>
2019-09-03 00:08:53 +0900

    complete kakao maps

--- src/components/DY.vue
+++ src/components/DY.vue
@@ -6,9 +6,7 @@
       </v-flex>
       <v-flex xs8>
         <input type="file" v-on:change="readFile" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"/>
-        <div id="staticMap">
-
-        </div>
+        <div id="map" style="width:100%;height:1000px;"></div>
       </v-flex>
       <v-flex xs4>
         <table v-if="excelData.Sheet1 != null">
@@ -46,8 +44,8 @@ export default {
   },
   methods: {
     isDY () {
-      var isOK = prompt('비밀번호를 입력하세요.', '')
-      if (isOK !== '1234') { this.$router.push('/') }
+      var isOK = prompt('나의 이름은?', '')
+      if (isOK !== '송문회') { this.$router.push('/') }
     },
     readFile (event) {
       const file = event.target.files[0]
@@ -68,39 +66,53 @@ export default {
       // this.searchLatLng()
     },
     searchLatLng (excel) {
-      // eslint-disable-next-line
+      /* eslint-disable */
       var geocoder = new kakao.maps.services.Geocoder()
       var cnt = excel.Sheet1.length
-      var markers = []
-      var marker = {}
+      var positions = []
+      var mapContainer = document.getElementById('map');
+      var mapOption = {
+          center: new kakao.maps.LatLng(37.450701, 126.570667),
+          level: 14
+      }
+      var map = new kakao.maps.Map(mapContainer, mapOption)
       for (var i = 0; i < cnt; i++) {
-        geocoder.addressSearch(excel.Sheet1[i][2], function (result, status) {
-          // eslint-disable-next-line
+        positions[i] = excel.Sheet1[i][2]
+      }
+      positions.forEach(function (addr, index) {
+        geocoder.addressSearch(addr, function (result, status) {
           if (status === kakao.maps.services.Status.OK) {
-            var x = result[0]['road_address']['x']
-            var y = result[0]['road_address']['y']
-            // eslint-disable-next-line
-            marker.position = new kakao.maps.LatLng(x, y)
-            marker.text = excel.Sheet1[i][0]
-            markers.push(marker)
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x)
+            // var marker = new kakao.maps.Marker({
+            //   map: map,
+            //   position: coords
+            // })
+            // var infowindow = new kakao.maps.InfoWindow({
+            //   content: '<div style="width:50px;">' + index + '</div>',
+            //   disableAutoPan: true
+            // })
+            // infowindow.open(map, marker);
+            var customOverlay = new kakao.maps.CustomOverlay({
+              map: map,
+              content: '<div style="background-color:white; border:2px solid red; font-weight:bold">' + index + '</div>',
+              position: coords
+            })
+            customOverlay.setMap(map)
           }
         })
-      }
-      var jsonMarker = JSON.stringify(markers)
-      console.log(jsonMarker)
-      drawMap(jsonMarker)
+      })
     }
   }
 }
-function drawMap (markers) {
-  /* eslint-disable */
-  var staticMapContainer = document.getElementById('staticMap'),
-    staticMapOption = {
-      level : 3,
-      marker : markers
-    }
-  var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption)
-}
+// function drawMap (markers) {
+//   /* eslint-disable */
+//   var staticMapContainer = document.getElementById('staticMap'),
+//     staticMapOption = {
+//       level : 3,
+//       marker : markers
+//     }
+//   var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption)
+// }
 </script>
 <style>
   ._tb {
