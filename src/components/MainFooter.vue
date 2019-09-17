be98baaf66d4f9d3ca0939b620927a72473d3a1c
sitaround <sitaround91@gmail.com>
2019-09-17 12:23:09 +0900

    remove footer content

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -5,7 +5,7 @@
         <Weather v-bind:latitude=latitude v-bind:longitude=longitude></Weather>
       </v-card-text>
       <v-card-text class="white--text pt-0">
-        <h1>간간히 Software Expert Academy 문제의 풀이와 리뷰, 알고리즘 문제를 풀면서 새로 알게 된 지식과 Effective Java 책을 공부하면서 알게 된 새로운 지식들을 심심할 때마다 포스팅하기 위한 블로그.</h1>
+        <h1></h1>
       </v-card-text>
       <v-card-actions class="grey darken-3 justify-center">
         &copy;2019 — <strong>MH</strong>
